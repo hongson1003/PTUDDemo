daaa9c9553cab7abfcfd11a988cbdf779e461df2
Hongsonnguyen <hongsonnguyen1003@gmail.com>
2024-01-08 21:52:20 +0700

    da them file connectdb.java

---
+++ src/connectdb/connectDB.java
@@ -0,0 +1,12 @@
+package connectdb;
+/*
+ * @description
+ * @author: Hong Son Nguyen
+ * @date: 02/01/2024
+ */
+
+public class connectDB {
+    public static void print(){
+        System.out.println("Connected db mogoo");
+    }
+}
